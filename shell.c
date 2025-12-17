542d30541c1a00b582f2a2ac3f4b964ac20e36d0
Ibrahim Agakishyev <ibrahim.aghakishyev@holbertonstudents.com>
2025-12-17 20:33:51 +0000

    Add simple shell with execute and env builtin

--- shell.c
+++ shell.c
@@ -1,8 +1,8 @@
 #include "shell.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/wait.h>
 
 extern char **environ;
@@ -11,11 +11,11 @@ void execute(char *line)
 {
 	char *args[64];
 	char *token;
-	int i = 0;
 	pid_t pid;
+	int i = 0;
 
 	token = strtok(line, " \t\n");
-	while (token && i < 63)
+	while (token != NULL && i < 63)
 	{
 		args[i++] = token;
 		token = strtok(NULL, " \t\n");
@@ -28,19 +28,22 @@ void execute(char *line)
 	if (strcmp(args[0], "exit") == 0)
 		exit(0);
 
-	/* Handle PATH resolution if needed */
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+			printf("%s\n", environ[i]);
+		return;
+	}
 
 	pid = fork();
 	if (pid == 0)
 	{
 		if (execve(args[0], args, environ) == -1)
 		{
-			perror("execve");
-			exit(EXIT_FAILURE);
+			perror("Error");
+			exit(1);
 		}
 	}
 	else
-	{
 		wait(NULL);
-	}
 }
